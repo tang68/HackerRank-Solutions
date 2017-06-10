7daceaaae9a587c0df693bc3ae46519f45e064b8
Ryan P. Tang <phattang1226@gmail.com>
2017-06-09 19:13:08 -0700

    Create Solution.java

--- Algorithms/Warmup/Time Conversion/Solution.java
+++ Algorithms/Warmup/Time Conversion/Solution.java
@@ -1,1 +1,29 @@
 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       String s = scan.nextLine();
+       String sub = s.substring(0, 2);
+       int hr = Integer.parseInt(sub);
+       if (s.substring(8,10).equals("PM")) {
+           if (hr == 12) {
+               System.out.println("12" + s.substring(2,8));
+           }
+           else {
+               hr = hr + 12;
+                System.out.println(hr + s.substring(2,8));
+           }       
+       }
+       else {
+           if (hr == 12)     
+                System.out.println("00" +  s.substring(2,8));
+           else
+                System.out.println("0" + hr + s.substring(2,8));
+       }
+        
+    }
+}
